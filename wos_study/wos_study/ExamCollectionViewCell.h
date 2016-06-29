cf449654a04b1b89458aaeb72106dc52714e78b8
SooHanJang <soohan.jang@worksmobile.com>
2016-06-29 18:52:47 +0900

    ## 메인 뷰컨트롤러 구현
    * 컬렉션뷰로 구성된 메인 뷰컨트롤러를 구현함.

---
+++ wos_study/wos_study/ExamCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  ExamCollectionViewCell.h
+//  wos_study
+//
+//  Created by Naver on 2016. 6. 29..
+//  Copyright © 2016년 worksmobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExamCollectionViewCell : UICollectionViewCell
+@property (strong, nonatomic) IBOutlet UILabel *label;
+
+@end
--- wos_study/wos_study/ViewController.m
+++ wos_study/wos_study/ViewController.m
@@ -14,14 +14,39 @@
 
 @implementation ViewController
 
+@synthesize examCells;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    examCells = [@[@"Ex01", @"Ex02", @"Ex03"] mutableCopy];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+
+    
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return examCells.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    ExamCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"ExamCell" forIndexPath:indexPath];
+    cell.label.text = examCells[indexPath.row];
+    
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"%ld", indexPath.row);
 }
 
 @end
